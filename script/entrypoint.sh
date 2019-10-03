bac0e7c861e9423bfd88e86e3812d704c80e324d
RensNaeff <50316842+RensNaeff@users.noreply.github.com>
2019-10-03 16:32:33 +0200

    Update entrypoint.sh

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Install ODBC-driver 17
-apt-get update \
-        && apt-get install -y curl apt-transport-https gnupg2 \
-        && curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
-        && curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list \
-        && apt-get update \
-        && ACCEPT_EULA=Y apt-get install -y msodbcsql17 mssql-tools \
-        && apt-get install unixodbc-dev -y
+#apt-get update \
+#        && apt-get install -y curl apt-transport-https gnupg2 \
+#        && curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
+#        && curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list \
+#        && apt-get update \
+#        && ACCEPT_EULA=Y apt-get install -y msodbcsql17 mssql-tools \
+#        && apt-get install unixodbc-dev -y
 
 TRY_LOOP="20"
 
