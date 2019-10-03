1f23cd627b021b99fdba770f4111bf1f1b0b20f8
RensNaeff <50316842+RensNaeff@users.noreply.github.com>
2019-10-03 13:20:24 +0200

    Update entrypoint.sh

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Install ODBC-driver 17
+apt-get update \
+        && apt-get install -y curl apt-transport-https gnupg2 \
+        && curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
+        && curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list \
+        && apt-get update \
+        && ACCEPT_EULA=Y apt-get install -y msodbcsql17 mssql-tools \
+        && apt-get install unixodbc-dev -y
+
 TRY_LOOP="20"
 
 : "${REDIS_HOST:="redis"}"
